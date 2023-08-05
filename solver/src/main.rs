84d9f201a27dbcf6cb4fd9aa1f81a1ac5ebbefd0
Kneasle <kneasle@gmail.com>
2023-08-05 17:17:12 +0100

    Newtypes for `{Cell,Region}Vec` and `CellBitmap`

--- solver/src/main.rs
+++ solver/src/main.rs
@@ -1,4 +1,7 @@
-use std::time::Instant;
+use std::{
+    ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, Not},
+    time::Instant,
+};
 
 fn main() {
     // let string = "1.1|2.2";
@@ -9,17 +12,17 @@ fn main() {
 
 #[derive(Debug)]
 struct Puzzle {
-    pip_count_per_cell: Vec<usize>,
+    pip_count_per_cell: CellVec<usize>,
     width: usize,
     height: usize,
 
-    adjacent_cell_bitmaps: Vec<u64>,
+    adjacent_cell_bitmaps: CellVec<CellBitmap>,
 }
 
 impl Puzzle {
     fn new(string: &str) -> Self {
         // Parse puzzle string
-        let mut pip_count_per_cell = Vec::new();
+        let mut pip_count_per_cell = CellVec::new();
         let mut width = None;
         for line in string.split('|') {
             // Add pip counts
@@ -38,16 +41,16 @@ impl Puzzle {
         let width = width.unwrap();
         let height = pip_count_per_cell.len() / width;
         // Compute adjacency bitmaps
-        let mut adjacent_cell_bitmaps = Vec::new();
+        let mut adjacent_cell_bitmaps = CellVec::new();
         for cy in 0..height as isize {
             for cx in 0..width as isize {
-                let mut bitmap = 0u64;
+                let mut bitmap = CellBitmap::empty();
                 for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
                     let x = cx + dx;
                     let y = cy + dy;
                     if 0 <= x && x < width as isize && 0 <= y && y < height as isize {
                         let index = y * width as isize + x;
-                        bitmap |= 1 << index;
+                        bitmap.set(CellIdx::new(index as usize));
                     }
                 }
                 adjacent_cell_bitmaps.push(bitmap);
@@ -63,11 +66,15 @@ impl Puzzle {
         }
     }
 
+    fn num_cells(&self) -> usize {
+        self.width * self.height
+    }
+
     fn total_num_pips(&self) -> usize {
         self.pip_count_per_cell.iter().sum::<usize>()
     }
 
-    fn print_bitmap(&self, bitmaps: &[(u64, char)]) {
+    fn print_bitmap(&self, bitmaps: &[(CellBitmap, char)]) {
         print_bitmaps(self.width, self.height, bitmaps);
     }
 }
@@ -107,7 +114,7 @@ impl<'data> SolvingContext<'data> {
 
     fn solve(&mut self) {
         let start = Instant::now();
-        for cell_idx in 0..self.puzzle.pip_count_per_cell.len() {
+        for (cell_idx, _) in self.puzzle.pip_count_per_cell.iter_enumerated() {
             PartialSolution::with_initial_inside(self, cell_idx).recursive_solve(self, 0);
         }
         println!(
@@ -124,24 +131,38 @@ impl<'data> SolvingContext<'data> {
 
 #[derive(Debug, Clone)]
 struct PartialSolution {
-    inside: u64,
-    outside: u64,
-    expandable_cells: u64,
+    inside: CellBitmap,
+    outside: CellBitmap,
+    expandable_cells: CellBitmap,
 
-    pips_in_current_region: usize,
+    pips_in_curr_in_region: usize,
+    out_regions: RegionVec<OutRegion>,
+    out_region_per_cell: CellVec<Option<RegionIdx>>,
+}
+
+#[derive(Debug, Clone)]
+struct OutRegion {
+    total_num_pips: usize,
 }
 
 impl PartialSolution {
     /// Create a `PartialSolution` with one single 'inside' cell at the given `cell_idx`, and with
     /// every cell below that marked as 'outside'.
-    fn with_initial_inside(ctx: &SolvingContext, cell_idx: usize) -> Self {
-        let inside = 1 << cell_idx;
-        let outside = inside - 1; // 1s up to the lowest 1 in inside
+    fn with_initial_inside(ctx: &SolvingContext, cell_idx: CellIdx) -> Self {
+        // Create indside/outside bitmaps
+        let inside_bits = 1 << cell_idx.index();
+        let inside = CellBitmap::from_bits(inside_bits);
+        let outside = CellBitmap::from_bits(inside_bits - 1); // 1s up to the lowest 1 in inside
+
         Self {
             inside,
             outside,
             expandable_cells: ctx.puzzle.adjacent_cell_bitmaps[cell_idx] & !outside,
-            pips_in_current_region: ctx.puzzle.pip_count_per_cell[cell_idx],
+
+            pips_in_curr_in_region: ctx.puzzle.pip_count_per_cell[cell_idx],
+            // TODO: Make these correct
+            out_regions: RegionVec::new(),
+            out_region_per_cell: index_vec::index_vec![None; ctx.puzzle.num_cells()],
         }
     }
 
@@ -169,12 +190,12 @@ impl PartialSolution {
                 }
             }
             None => {
-                if self.pips_in_current_region == 0
-                    || ctx.puzzle.total_num_pips() % self.pips_in_current_region != 0
+                if self.pips_in_curr_in_region == 0
+                    || ctx.puzzle.total_num_pips() % self.pips_in_curr_in_region != 0
                 {
                     return;
                 }
-                println!("FOUND SOLUTION OF {}:", self.pips_in_current_region);
+                println!("FOUND SOLUTION OF {}:", self.pips_in_curr_in_region);
                 self.print_assignment_str(ctx);
                 println!();
                 // TODO: Add a twist and continue searching
@@ -186,10 +207,10 @@ impl PartialSolution {
         };
     }
 
-    fn cell_to_expand(&self, ctx: &SolvingContext) -> Option<usize> {
-        let lowest_unexpanded_cell = self.expandable_cells.trailing_zeros() as usize;
+    fn cell_to_expand(&self, ctx: &SolvingContext) -> Option<CellIdx> {
+        let lowest_unexpanded_cell = self.expandable_cells.lowest_true_bit().index();
         if lowest_unexpanded_cell < ctx.puzzle.pip_count_per_cell.len() {
-            Some(lowest_unexpanded_cell)
+            Some(CellIdx::new(lowest_unexpanded_cell))
         } else {
             None
         }
@@ -199,7 +220,7 @@ impl PartialSolution {
     fn assign_cell(
         &mut self,
         ctx: &SolvingContext,
-        cell_idx: usize,
+        cell_idx: CellIdx,
         assignment: Assignment,
     ) -> bool {
         // println!("Expanding cell {cell_idx} with {assignment:?}");
@@ -209,17 +230,17 @@ impl PartialSolution {
             Assignment::Outside => &mut self.outside,
             Assignment::Inside => &mut self.inside,
         };
-        *assigned_bitmap |= 1 << cell_idx;
+        assigned_bitmap.set(cell_idx);
         // Update which cells are expandable
-        self.expandable_cells &= !(1 << cell_idx); // This cell can't be expanded more
+        self.expandable_cells.unset(cell_idx); // This cell can't be expanded more
         if assignment == Assignment::Inside {
-            self.pips_in_current_region += ctx.puzzle.pip_count_per_cell[cell_idx];
+            self.pips_in_curr_in_region += ctx.puzzle.pip_count_per_cell[cell_idx];
             // println!(
             //     "{} (biggest possible) vs {} (in region)",
             //     ctx.biggest_required_pip_count(),
             //     self.pips_in_current_region
             // );
-            if self.pips_in_current_region > ctx.biggest_required_pip_count() {
+            if self.pips_in_curr_in_region > ctx.biggest_required_pip_count() {
                 // This region includes so many pips that it can't provide a new solution
                 // println!("Rejecting solution because it exceeds maximum count");
                 return false;
@@ -230,7 +251,7 @@ impl PartialSolution {
             self.expandable_cells |= new_adjacent_cells & !already_assigned_cells;
         }
 
-        assert_eq!(self.inside & self.outside, 0); // No cell can be both inside and outside the line
+        assert_eq!(self.inside & self.outside, CellBitmap::empty()); // No cell can be both inside and outside the line
 
         true
     }
@@ -247,14 +268,14 @@ enum Assignment {
     Inside,
 }
 
-fn print_bitmaps(width: usize, height: usize, bitmaps: &[(u64, char)]) {
+fn print_bitmaps(width: usize, height: usize, bitmaps: &[(CellBitmap, char)]) {
     let mut string = String::new();
     'idx_loop: for idx in 0..width * height {
         if idx > 0 && idx % width == 0 {
             string.push('\n');
         }
         for &(bitmap, char) in bitmaps {
-            if bitmap & (1 << idx) != 0 {
+            if bitmap.get(CellIdx::new(idx)) {
                 string.push(char);
                 continue 'idx_loop;
             }
@@ -263,4 +284,77 @@ fn print_bitmaps(width: usize, height: usize, bitmaps: &[(u64, char)]) {
         string.push('-');
     }
     println!("{string}");
+}
+
+index_vec::define_index_type! { pub struct CellIdx = usize; }
+type CellVec<T> = index_vec::IndexVec<CellIdx, T>;
+
+index_vec::define_index_type! { pub struct RegionIdx = usize; }
+type RegionVec<T> = index_vec::IndexVec<RegionIdx, T>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct CellBitmap {
+    bits: u64,
+}
+
+impl CellBitmap {
+    fn empty() -> Self {
+        Self::from_bits(0)
+    }
+
+    fn from_bits(bits: u64) -> Self {
+        Self { bits }
+    }
+
+    fn get(self, cell: CellIdx) -> bool {
+        self.bits & (1 << cell.index()) != 0
+    }
+
+    fn set(&mut self, cell: CellIdx) {
+        self.bits |= 1 << cell.index();
+    }
+
+    fn unset(&mut self, cell: CellIdx) {
+        self.bits &= !(1 << cell.index());
+    }
+
+    fn lowest_true_bit(self) -> CellIdx {
+        CellIdx::new(self.bits.trailing_zeros() as usize)
+    }
+}
+
+impl Not for CellBitmap {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        CellBitmap::from_bits(!self.bits)
+    }
+}
+
+impl BitOr for CellBitmap {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        CellBitmap::from_bits(self.bits | rhs.bits)
+    }
+}
+
+impl BitOrAssign for CellBitmap {
+    fn bitor_assign(&mut self, rhs: Self) {
+        self.bits |= rhs.bits;
+    }
+}
+
+impl BitAnd for CellBitmap {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        CellBitmap::from_bits(self.bits & rhs.bits)
+    }
+}
+
+impl BitAndAssign for CellBitmap {
+    fn bitand_assign(&mut self, rhs: Self) {
+        self.bits &= rhs.bits;
+    }
 }
